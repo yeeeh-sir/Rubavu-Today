0107ea366df282ae5def470153f4d3d833195900
YeehSir <tuyishimvalens142@gmail.com>
2026-08-25 10:30:25 +0200

    Update Rubavu Today frontend

--- Rubavu-Today/src/index.js
+++ Rubavu-Today/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rubavuIcon from './Rubavu.jpeg';
+
+document.querySelector('link[rel="icon"]')?.setAttribute('href', rubavuIcon);
+document.querySelector('link[rel="apple-touch-icon"]')?.setAttribute('href', rubavuIcon);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
